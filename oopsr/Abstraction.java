275a03322c5b3d51faea600c4c9a4464b92f8a36
Ankit-cs <ankitcareer018@gmail.com>
2026-05-15 17:02:28 +0530

    updated abstraction code file

--- oopsr/Abstraction.java
+++ oopsr/Abstraction.java
@@ -1,5 +1,19 @@
 package SystemDesign2026.oopsr;
 import java.util.*;
+
+/*
+Car Interface --> Act as an interface for Outsiude world to operate the car.
+This interface tells 'WHAT' all it can do rather then 'HOW' it does that.
+Since this is an interface we cannot directly create Objects of this. We
+need to implement it first and then that child class will have the responsibility to
+provide implementation details of all the methods in the interface.
+
+In our real world example of Car, imagine you sitting in the car and able to operate
+the car (startEngine, accelerate, brake, turn) just by pressing or moving some
+pedals/buttons/stearing wheel etc. You dont need to know how these things work, and
+also they are hidden under thre hood.
+This Interface 'Car' denotes that (pedals/buttons/stearing wheel etc).
+*/
 interface Car{
     void startEngine();
     void shiftGear(int gear);
@@ -7,6 +21,21 @@ interface Car{
     void breakEngine();
     void stopEngine();
 }
+
+
+/*
+This is a Concrete class (A class that provide implementation details of an interface/abstract class).
+Now anyone can make an Object of 'SportsCar' and can assign it to 'Car' reference.
+(See main method for this)
+
+In our real world example of Car, as you cannot have a real car by just having its body only
+(all these buttons or pedals). You need to have the actual implementation of 'What' happens
+when we press these buttons. 'SportsCar' class denotes that actual implementation.
+
+Hence we can concude, to denote a real world car in programming we created 2 classes.
+One to deonte all the user-interface like pedals, buttons, stearing wheels etc ('Car' interface).
+And another one to denote the actual car with all the implementations of these buttons (SportsCar' class).
+ */
 
 class mySportsCar implements Car{
      String brand;
---
+++ oopsr/Encapsulation.java
@@ -0,0 +1,14 @@
+package SystemDesign2026.oopsr;
+import java.util.*;
+
+iterface car{
+
+}
+class mySportsCar implements car{
+
+}
+public class Encapsulation {
+    static void main() {
+
+    }
+}
