f23baf22504557d2fccc336922999ab39cdf8069
Ankit-cs <ankitcareer018@gmail.com>
2026-06-11 17:24:30 +0530

    setup user model

---
+++ _foodDelivery/models/User.java
@@ -0,0 +1,30 @@
+package models;
+
+public class User{
+    private int UserId;
+    private String name;
+    private String address;
+    private Cart cart;
+
+    public User(int Userid,String name,String address){
+        this.UserId=Userid;
+        this.name=name;
+        this.address=address;
+        this.cart=new Cart();
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name=n;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String a){
+        address=a;
+    }
+    public Cart getCart(){
+        return cart;
+    }
+}
