d0396b4e53fce1170220ed73b970d4f76218ebc9
Ankit-cs <ankitcareer018@gmail.com>
2026-06-11 21:00:58 +0530

    created Order Manger class

---
+++ _foodDelivery/managers/OrderManager.java
@@ -0,0 +1,28 @@
+package managers;
+
+import java.util.ArrayList;
+import java.util.List;
+import models.Order;
+
+public class OrderManager{
+    private List<Order> orders = new ArrayList<>();
+    private static OrderManager instance = null;
+
+    private OrderManager(){ }
+
+    public static OrderManager getInstance(){
+        if(instance==null){
+            instance=new OrderManager();
+        }
+        return instance;
+    }
+    public void addOrder(Order order){
+        orders.add(order);
+    }
+    public void listOrders(){
+        System.out.println("\n---All Orders---");
+        for(Order order:orders){
+            System.out.println(order.getType()+" order for "+order.getUser().getName()+" | Total: \u20B9"+order.getTotal()+" At: "+order.getScheduled());
+        }
+    }
+}
