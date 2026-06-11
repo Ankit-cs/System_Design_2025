5017f33208489b3c9a3603697fe6dfb31cf7e800
Ankit-cs <ankitcareer018@gmail.com>
2026-06-11 20:56:20 +0530

    solved imprt bugs

--- _foodDelivery/factories/NowOrderFactory.java
+++ _foodDelivery/factories/NowOrderFactory.java
@@ -1,31 +1,30 @@
 package factories;
 
-import java.utils.List;
+import java.util.List;
 import models.*;
-import strategies.*;
-import utils.*;
+import strategies.PaymentStrategy;
+import utils.TimeUtils;
 
 public class NowOrderFactory implements OrderFactory{
-   @Override
-   public Order createOrder(User user,Cart cart,Restaurant restaurant,
-    List<MenuItem> menuItems,PaymentStrategy paymentStrategy,double totalCost,String orderType){
-    Order order=null;
-    if(orderType.equals("Delivery")){
-    DeliveryOrder deliveryOrder=new DeliveryOrder();
-    deliveryOrder.setUserAddress(user.getAddress());
-    order=deliveryOrder;
-   }
-   else{
-      PickupOrder pickupOrder=new PickupOrder();
-      pickupOrder.setRestaurantAddress(restaurant.getLocation());
-      order=pickupOrder;
-   }
-   order.setUser(user);
-   order.setRestaurant(restaurant);
-   order.setItems(menuItems);
-   order.setPaymentStrategy(paymentStrategy);
-   order.setSheduled(TimeUtils.getCurrentTime());
-   order.setTotal(totalCost);
-   return order;
-    }
+    @Override
+    public Order createOrder(User user,Cart cart,Restaurant restaurant,
+      List<MenuItem> menuItems,PaymentStrategy paymentStrategy,double totalCost,String orderType){
+      Order order=null;
+      if(orderType.equalsIgnoreCase("Delivery")){
+            DeliveryOrder deliveryOrder=new DeliveryOrder();
+            deliveryOrder.setUserAddress(user.getAddress());
+            order=deliveryOrder;
+      } else {
+         PickupOrder pickupOrder=new PickupOrder();
+         pickupOrder.setRestaurantAddress(restaurant.getLocation());
+         order=pickupOrder;
+      }
+    order.setUser(user);
+    order.setRestaurant(restaurant);
+    order.setItems(menuItems);
+    order.setPaymentStrategy(paymentStrategy);
+    order.setScheduled(TimeUtils.getCurrentTime());
+    order.setTotal(totalCost);
+    return order;
+      }
 }
