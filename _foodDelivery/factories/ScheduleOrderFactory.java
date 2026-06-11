63ebb209628d307561f2c798ae5ae091231ba1e1
Ankit-cs <ankitcareer018@gmail.com>
2026-06-11 20:56:39 +0530

    solved imprt bugs

--- _foodDelivery/factories/ScheduleOrderFactory.java
+++ _foodDelivery/factories/ScheduleOrderFactory.java
@@ -1,14 +1,14 @@
 package factories;
 
-import java.utils.List;
+import java.util.List;
 import models.*;
-import strategies.*;
-
+import strategies.PaymentStrategy;
+import utils.TimeUtils;
 
 public class ScheduleOrderFactory implements OrderFactory{
     private String scheduleTime;
-    
-    private ScheduleOrderFactory(String scheduleTime){
+
+    public ScheduleOrderFactory(String scheduleTime){
         this.scheduleTime=scheduleTime;
     }
 
@@ -16,21 +16,20 @@ public class ScheduleOrderFactory implements OrderFactory{
    public Order createOrder(User user,Cart cart,Restaurant restaurant,
     List<MenuItem> menuItems,PaymentStrategy paymentStrategy,double totalCost,String orderType){
     Order order=null;
-    if(orderType.equals("Delivery")){
-    DeliveryOrder deliveryOrder=new DeliveryOrder();
-    deliveryOrder.setUserAddress(user.getAddress());
-    order=deliveryOrder;
-   }
-   else{
+    if(orderType.equalsIgnoreCase("Delivery")){
+        DeliveryOrder deliveryOrder=new DeliveryOrder();
+        deliveryOrder.setUserAddress(user.getAddress());
+        order=deliveryOrder;
+    } else {
       PickupOrder pickupOrder=new PickupOrder();
       pickupOrder.setRestaurantAddress(restaurant.getLocation());
       order=pickupOrder;
-   }
+    }
    order.setUser(user);
    order.setRestaurant(restaurant);
    order.setItems(menuItems);
    order.setPaymentStrategy(paymentStrategy);
-   order.setSheduled(TimeUtils.getCurrentTime());
+   order.setScheduled(this.scheduleTime != null ? this.scheduleTime : TimeUtils.getCurrentTime());
    order.setTotal(totalCost);
    return order;
     }
